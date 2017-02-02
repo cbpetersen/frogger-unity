6f24e7604e4af44cdb33d6dd40f4c887b11f6fed
Christoffer Bo Petersen <cb.petersen@gmail.com>
2017-02-02 22:03:27 +0100

    :lipstick: Add jump animation to player frog

--- Assets/Scripts/GameConfig.cs
+++ Assets/Scripts/GameConfig.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class GameConfig : MonoBehaviour
 {
-//	public float PlayerMoveDistance = 0.5f;
-//	public float TimeBetweenMoves = 0.15f;
-//	public float EnemyMoveDistance = 0.5f;
-//	public float EnemyMoveMoveCoolDown = 0.5f;
 	public float PlayerMoveDistance;
 	public float TimeBetweenMoves;
 	public float EnemyMoveDistance;
 	public float EnemyMoveMoveCoolDown;
 }
-
+	
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,33 +4,45 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Player : BaseMonoBehaviour {
+	public Sprite[] sprites;
 	public Vector2 HorizontalBoundery;
 	public Vector2 VerticalBoundery;
-
-	private bool alive;
 	public float lastMove = 0f;
 
-	private void Move() {
-	
+	private SpriteRenderer render;
+	private bool alive;
+	private int spriteIndex;
+
+	private void Jump(int moveDirection) {
+		this.spriteIndex = moveDirection;
 	}
 
 	void Start () {
 		base.Start ();
+		render = GetComponent<SpriteRenderer> ();
 		alive = true;
 	}
 
 	void Update () {
 		if (alive && Time.unscaledTime - gameConfig.TimeBetweenMoves > lastMove) {
+
+
 			Vector3 v = Vector3.left * Input.GetAxisRaw ("Horizontal") * Time.deltaTime;
 			if (v.x > 0) {
 				if (transform.position.x > -2) {
-					lastMove = Time.unscaledTime;		
+					lastMove = Time.unscaledTime;
 					transform.Translate (new Vector2 (-gameConfig.PlayerMoveDistance, v.y));
+					render.sprite = sprites [7];
+					spriteIndex = 7;
+					return;
 				}
 			} else if (v.x < 0) {
 				if (transform.position.x < 2) {
 					transform.Translate (new Vector2 (gameConfig.PlayerMoveDistance, v.y));
 					lastMove = Time.unscaledTime;
+					render.sprite = sprites [3];
+					spriteIndex = 3;
+					return;
 				}
 			}
 
@@ -39,12 +51,22 @@ public class Player : BaseMonoBehaviour {
 				if (transform.position.x > -2) {
 					lastMove = Time.unscaledTime;		
 					transform.Translate (new Vector2 (v.x, -gameConfig.PlayerMoveDistance));
+					render.sprite = sprites [5];
+					spriteIndex = 5;
+					return;
 				}
 			} else if (v.x < 0) {
 				if (transform.position.x < 2) {
 					transform.Translate (new Vector2 (v.x, gameConfig.PlayerMoveDistance));
 					lastMove = Time.unscaledTime;
+					render.sprite = sprites [1];
+					spriteIndex = 1;
+					return;
 				}
+			}
+
+			if (spriteIndex % 2 == 1) {
+				render.sprite = sprites [--spriteIndex];
 			}
 		}
 	}
