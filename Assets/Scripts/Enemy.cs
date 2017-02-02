23fb311056a30dfadcd58d14e9450c2d9f7d0321
Christoffer Bo Petersen <cb.petersen@gmail.com>
2017-02-02 21:01:47 +0100

    :art: Make Enemies reset horizontally

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Enemy : BaseMonoBehaviour {
 	private float movedAt = 0;
 	private int moveDirection = 1;
+	private Vector3 startPosition;
+
+	public void init(Vector2 pos, int moveDirection) {
+		startPosition = new Vector3 (pos.x, pos.y);
+	}
 
 	void Start () {
 		base.Start ();
+		startPosition = transform.position;
 		moveDirection = Mathf.FloorToInt (Random.value) == 0 ? 1 : -1;
 	}
 
@@ -16,5 +22,9 @@ public class Enemy : BaseMonoBehaviour {
 			transform.Translate (new Vector2 (-gameConfig.EnemyMoveDistance, 0));
 			movedAt = Time.unscaledTime;
 		}
+	}
+
+	void OnBecameInvisible() {
+		transform.position = startPosition;
 	}
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
-
+	public int lvl;
+	public Enemy[] EnemyTypes;
+	public GameObject Croc;
 	// Use this for initialization
 	void Start () {
-		
+		Instantiate(Croc, new Vector3(1, 0.5f, 0), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
